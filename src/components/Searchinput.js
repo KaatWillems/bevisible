dacbecb7026cf777b3196e368775abbe4988d3e7
charlottelotte2021 <charlotteguen@gmail.com>
2022-05-11 10:56:05 +0200

    first components

---
+++ src/components/Searchinput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Searchinput = (props) => {
+  return (
+    <div></div>
+  )
+}
+
+export default Searchinput;
---
+++ src/components/Startpage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Startpage = (props) => {
+  return (
+    <div></div>
+  )
+}
+
+export default Startpage;
