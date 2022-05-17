3bd55d67a8c8dd202e0b8b7dbea15ecc53caf73b
charlottelotte2021 <charlotteguen@gmail.com>
2022-05-17 13:51:27 +0200

    tags and startpage, a lot of mapmap map

--- src/components/Startpage.js
+++ src/components/Startpage.js
@@ -5,14 +5,28 @@ import { Link } from "react-router-dom";
 import Card from "./Card"
 import Filter from "./features/Filter"
 import Navbar from "./features/Navbar"
+import profiledata from './profiledata.json'
+import pic1 from '../images/profile1.png'
+
+// import Cardslist from "./Cardslist"
+// let Cardslist = profiledata.map((card, index) => {
+//   return <Card cardinfo={card} />
+// })
 
 const Startpage = (props) => {
+
+ let Cardslist = profiledata.map((card, index) => {
+  return <Card cardinfo={card} key={`card_nbr_${index}`}  />
+})
+
+
   return (
     <>
     
     <Header />
-    
-    <Card />
+    <div className="cards-container"> 
+    {Cardslist}
+    </div>
     <Navbar />
     </>
   )
