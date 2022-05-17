d5bb9b589112ec514a5cd67d41ac77eb5dcd6077
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-05-17 13:51:25 +0200

    register and login

--- src/components/features/Loginform.js
+++ src/components/features/Loginform.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Loginform = ({ className, type, id, placeholder, name, value, onChange  }) => {
+const Loginform = ({ className, type, id,  placeholder, name, value, onChange  }) => {
   return (
     
 				<input 
