8dc6283077d91f61462d877650292a391cd925d2
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-05-27 11:33:56 +0200

    does not work yet

--- src/components/features/Navbar.js
+++ src/components/features/Navbar.js
@@ -3,15 +3,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Navbar() {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const getAllProfiles = () => {
+      fetch("https://bevisible-backend.herokuapp.com/user/all", {
+        // mode: 'no-cors',
+        method:'GET',
+         headers:{
+        //   'Content-Type':'application/json'
+        'Authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyO…zIwfQ.-1Gj84_ZZ7BucNBSm9i90gspoMhge3I7be9E6T5hO6Y"
+         }, 
+        // body: JSON.stringify({email: user, password: pwd})
+      })
+      .then(response=> response.json())
+      .then((data) => {
+      // let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyO…zIwfQ.-1Gj84_ZZ7BucNBSm9i90gspoMhge3I7be9E6T5hO6Y"
+      //   if (token) {
+      
+      //   navigate("/home", { replace: true });
+        
+      //   }
+        console.log(data)
+      })
+
+    }
+    getAllProfiles()
+  }
+
+
   return (
     <div className='navbar-container'>
 
 
         
-        <Link to="/home" className='navbar-icon explore-icon'>
+        <Link to="/home" className='navbar-icon explore-icon' onClick={handleSubmit}>
             <FontAwesomeIcon icon={faSearch} />
             <p className='text-icon-nav'>Explore</p>
             </Link>    
